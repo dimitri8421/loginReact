051389eff543a1f6f2425d381991bc54c2551703
William Patrick <117779516+dimitri8421@users.noreply.github.com>
2022-12-06 14:42:36 -0400

    Delete src directory

--- src/components/Circles.jsx
+++
@@ -1,5 +0,0 @@
-export function Circles(props) {
-    return (
-        <div className="circles" style={props.style}></div>
-    );
-}
--- src/components/Signup.jsx
+++
@@ -1,5 +0,0 @@
-export function Signup(){
-    return (
-        <div className="title2">Dont'te have an account? <b>Signup</b></div>
-    );
-}
